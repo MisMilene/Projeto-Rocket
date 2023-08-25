6f9f7aa2772c0f4dcca611c7789da4f7d53250ee
Milene Pires <milenepirescastro@gmail.com>
2023-08-25 19:45:44 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+   const html=document.documentElement
+  
+   /*isso faz o botao mover e faz a tela mudar comando de javascript*/
+   /*
+   if(html.ClassList.constains("ligth")){
+   html.ClassList.remove("ligth");
+   }else {
+  
+    html.classList.add("ligth")
+   }
+
+   /* ou essa funçao pode ser feita de outra forma */
+          html.classList.toggle("ligth")
+    
+}
